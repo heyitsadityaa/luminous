69f572360d17c20c5053f6110d032544b4be1abb
Aditya Prakash <prakaashaditya@gmail.com>
2026-04-04 03:15:22 +0530

    fix: add suppressHydrationWarning to RootLayout HTML tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body className={`${fontSans.variable} ${fontSerif.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
